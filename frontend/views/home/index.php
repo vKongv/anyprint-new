eb3cd66a726d4f1ff0bc458d12cc43480dd6ec7c
duduvien <DuduVien@viviens-MBP.lan>
2016-07-30 00:15:47 +0800

    Testing on branch switching

--- frontend/views/home/index.php
+++ frontend/views/home/index.php
@@ -4,6 +4,5 @@
 <h1>home/index</h1>
 
 <p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
+    Testing on branch switching <code><?= __FILE__; ?></code>.
 </p>
